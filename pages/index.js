5be0d5c363ecb2ad13a431fa94b724cdb574490e
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-01 07:10:06 -0500

    add MyTable to home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+// Next.js Imports
 import Head from 'next/head'
+// Custom Components
+import MyTable from '../components/MyTable';
+// CSS Module Styling
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -7,6 +11,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="An example of a meta description. These show up in search engine results." />
       </Head>
 
       <main className={styles.main}>
@@ -19,35 +24,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <MyTable />
       </main>
 
       <footer className={styles.footer}>
