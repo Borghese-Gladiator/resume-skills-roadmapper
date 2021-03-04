a74bc658ca0cbf895fb9074db80661ab6c3980b9
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-04 01:23:57 -0500

    add navbar & footer to layout

--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function DefaultLayout({ children }) {
   const [counter, setCounter] = useState(0);
 
   return (
-    <>
-      <p>      
+    <div id="root">
+      <p>
         <button onClick={() => setCounter(counter + 1)}>
           Clicked {counter} Times
         </button>
       </p>
-
-      {children}
-    </>
+      <Navbar />
+      <main id='content'>
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
