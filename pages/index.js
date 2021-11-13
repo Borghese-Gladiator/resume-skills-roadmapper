4091d319930439792a9b9ef0830c2ce55cfce418
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-13 16:51:34 -0500

    Initialize Geist UI

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Head from 'next/head';
-import Link from 'next/link'
+import Link from 'next/link';
+import { Page, Text, Image, Display, Button, Grid } from '@geist-ui/react'
 
 export default function Home() {
+  const gh = 'https://github.com/geist-org/react'
+  const docs = 'https://react.geist-ui.dev'
+  const redirect = (url) => {
+    window.open(url)
+  }
+
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Geist UI with NextJS</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="An example of a meta description. These show up in search engine results." />
       </Head>
-
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Resume Skills Builder</a>
-        </h1>
+      <Page dotBackdrop width="800px" padding={0}>
         <ul>
           <li>
             <Link
@@ -37,7 +39,37 @@ export default function Home() {
             </Link>
           </li>
         </ul>
-      </main>
+        <Display
+          title="Geist UI"
+          caption={
+            <>
+              Welcome to{' '}
+              <Text span b>
+                Geist UI
+              </Text>{' '}
+              and start learning more !
+            </>
+          }>
+          <Image src="/geist-banner.png" draggable={false} />
+        </Display>
+        <Grid.Container justify="center" gap={3} mt="100px">
+          <Grid xs={20} sm={7} justify="center">
+            <Button
+              shadow
+              type="secondary-light"
+              width="100%"
+              onClick={() => redirect(gh)}>
+              GitHub Repo
+            </Button>
+          </Grid>
+          <Grid xs={0} sm={3} />
+          <Grid xs={20} sm={7} justify="center">
+            <Button width="100%" onClick={() => redirect(docs)}>
+              Documentation Site
+            </Button>
+          </Grid>
+        </Grid.Container>
+      </Page>
     </div>
   )
 }
