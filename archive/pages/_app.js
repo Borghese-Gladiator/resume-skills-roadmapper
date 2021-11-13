a98bbe2c3908dc2ab6c7fc4e61856b248071ea26
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-13 16:42:36 -0500

    Archive content

---
+++ archive/pages/_app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }) {
+  const Layout = Component.Layout ? Component.Layout : React.Fragment;
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
