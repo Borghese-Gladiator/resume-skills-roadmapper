71db7f7db0099227bd83e9595f0e89996907b6f1
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-14 20:33:45 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-import { Grid, Card } from '@geist-ui/react';
+import { Grid, Card, Text } from '@geist-ui/react';
 
 export default function Header() {
   return (
-    <Grid xs={24}><Card shadow width="100%" height="50px" /></Grid>
+    <Grid xs={24}>
+      <Card shadow width="100%" height="50px">
+        <Text h4>
+          Resume Skill Tree
+        </Text>
+      </Card>
+    </Grid>
   )
 }
