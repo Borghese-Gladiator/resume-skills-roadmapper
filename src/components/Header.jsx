cb0dfbb15021a2242db026eb4ae2aa7d3aa0338f
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-25 23:46:59 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { Grid, Card, Text } from '@geist-ui/react';
+import { useToasts, Grid, Card, Text, Button } from '@geist-ui/react';
 
 export default function Header() {
+  const [toasts, setToast] = useToasts()
+  const click = () => setToast({ text: 'Resume Skill Tree shows essential skills in a web UI instead of tossing it all in a Google Docs.' })
   return (
     <Grid xs={24}>
-      <Card shadow width="100%" height="50px">
+      <Card type={"success"} shadow width="100%" height="50px">
         <Text h4>
-          Resume Skill Tree
+          Resume Skill Tree{" "}
+          <Button type="success" ghost auto scale={0.7} onClick={click}>Desc</Button>
         </Text>
       </Card>
     </Grid>
