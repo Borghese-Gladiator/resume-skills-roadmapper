3c49f37dfdc954e7148fb2d4fcfbbe3268d9c908
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-25 23:54:17 -0500

    Adjust height and add text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
   const [toasts, setToast] = useToasts()
   const click = () => setToast({ text: 'Resume Skill Tree shows essential skills in a web UI instead of tossing it all in a Google Docs.' })
   return (
-    <Grid xs={24}>
-      <Card type={"success"} shadow width="100%" height="50px">
+    <Grid xs={24} height="100px">
+      <Card type={"success"} shadow width="100%">
         <Text h4>
           Resume Skill Tree{" "}
-          <Button type="success" ghost auto scale={0.7} onClick={click}>Desc</Button>
+          <Button type="success" ghost auto scale={0.7} onClick={click}>Show Desc</Button>
         </Text>
       </Card>
     </Grid>
